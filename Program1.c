939475979f9c7b20a5a2ab3c631fc84179a72456
Isra41 <perezvicenteisrael2013@gmail.com>
2023-10-22 03:00:48 -0600

    Programa4 agregado, Program1 modificado

--- Program1.c
+++ Program1.c
@@ -14,9 +14,22 @@ void function2(void)
     printf("Esta es la función para CMD2");
 }
 
+/* Esta función es para implementar
+    funcionalidad del periférico 3*/
+void function2(void)
+{
+    printf("Esta es la función para CMD3");
+    int a;
+    a = 4/5*5 + 3;
+}
+
 int main()
 {
     printf("Hola desde VSCode");
     printf("Hola desde VSCode");
+
+    /*Ya se puso la configuracion de LPM*/
+    function("Llamada de funcion");
+    a = b*3 - 4;
     return 0;
 }
---
+++ Program4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void function()
+{
+    printf("Publish");
+}
+
+int main()
+{
+    printf("Code for program 4");
+    return 0;
+}
