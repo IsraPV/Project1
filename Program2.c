def252884f42a12ff8f000e8e7a1e2fce4020b47
Isra41 <perezvicenteisrael2013@gmail.com>
2023-10-22 01:01:10 -0600

    Eliminando Program2.c

--- Program2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Hola desde VSCode 2");
-    printf("Hola desde VSCode por segunda vez");
-    printf("Hola desde VSCode por tercera vez");
-    return 0;
-}
